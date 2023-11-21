25f8ccecef5427033a2bc839db9ec8dab6d29738
Rajan-007 <aravinthrajan034@gmail.com>
2023-11-21 09:18:35 +0530

    delete button

--- app/components/Task.js
+++ app/components/Task.js
@@ -11,10 +11,10 @@ const Task = (props) => {
   }
   return (
    <View>
-    <TouchableOpacity className='bg-[#f4f4f2] mt-10 mx-8 py-3 rounded-lg flex flex-row justify-around items-center 'onPress={() => completeTask(index)}>
+    <TouchableOpacity className='bg-[#f4f4f2] mt-10 mx-8 py-3 rounded-lg flex flex-row justify-around items-center '>
         <View className='h-5 w-5 bg-black rounded-md'></View>
         <Text className=' '>{props.text}</Text>
-        <View className='h-5 w-5 border-4 border-black rounded-md'></View>
+        <View className='h-5 w-5 border-4 border-black rounded-md' onPress={() => completeTask(index)}></View>
     </TouchableOpacity>
    </View>
   )
